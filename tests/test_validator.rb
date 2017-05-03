05d9c3ab9739f28dd4625137170571153115d7e8
Johann Eloff <johanne@urup.com>
2017-05-03 22:21:01 +0200

    Covered most data consistency formats

--- tests/test_validator.rb
+++ tests/test_validator.rb
@@ -10,17 +10,38 @@ class TestSimpleNumber < Test::Unit::TestCase
     assert_equal(data, is_consistent?(data)) 
   end
 
+  def test_instrucitons_is_consistent_1
+    data = ["3 3", "0 0 N", "MMMabcLMMM"]
+    assert_equal(data, is_consistent?(data)) 
+  end	
+
   def test_instrucitons_empty
     assert_raise(RuntimeError) do
       is_consistent? nil
-    end  
-	    
+    end 
   end
   
   def test_instructions_is_not_conisten_length
+    data = ["0 0", "MMMLMMM"]
+    assert_equal(nil, is_consistent?(data))    
   end
 
   def test_instrucitons_is_not_consistent_format
+    data = ["3 3", "0 0 N", "abc"]
+    assert_equal(nil, is_consistent?(data)) 
   end	  
  
+  def test_instrucitons_is_not_consistent_format_1
+    data = ["3 3", "0 0N", "MMMLMMMRMMM"]
+    assert_equal(nil, is_consistent?(data)) 
+  end	  
+ 
+  def test_instrucitons_is_not_consistent_format_2
+    data = ["33", "0 0 N", "MMMLMMMRMMM"]
+    assert_equal(nil, is_consistent?(data)) 
+  end	  
+ 
+
+
+
 end
