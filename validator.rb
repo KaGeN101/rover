cb8693454206b48f75b9802106afef9c3ce11b5e
Johann Eloff <johanne@urup.com>
2017-05-03 22:14:09 +0200

    Validates instrucitons set received

--- validator.rb
+++ validator.rb
@@ -5,9 +5,24 @@ module RoverData
     # Checks the instructions and returns it if valid
     # Will print error and fall to nil if not valid returning false in essence  
     if instructions.length == 3
-      return instructions	    
+      # Use regualar expression to test the formats in order	     
+      if instructions[0] =~ /\d\s\d/	    
+	if instructions[1] =~ /\d\s\d\s[N|E|S|W]/
+	  # This will match any combinaitons of valid more commands as long as they are there first 
+	  # and can be mixed with garbage will filter out the valid   	
+	  if instructions[2] =~ /(M|L|R)+/ 
+            return instructions
+          else
+            puts "Invalid move commad set, must be combinations of M|L|R"		  
+	  end
+	else
+	  puts "Initial postion and direction not in the right format, must be 'x y N|E|S|W'"
+	end  
+      else
+        puts "Grid size not in the right format must be 'x y'"	
+      end	
     else
-      puts "Instrucitons set does nto have the correct length"      
+      puts "Instructions set does nto have the correct length"      
     end      	    
   end  
 
